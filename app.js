8a1dcbd49002e570d23c1aeca3d253a11f71c763
kimgura <oli999@naver.com>
2025-05-15 11:38:24 +0900

    기능추가함

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.get('/', (req, res) => {
   res.send(`
     <h1>Hello from this NodeJS app!</h1>
     <p>Try sending a request to /error and see what happens</p>
+    <p>장희주 바보!</p>
   `);
 });
 
